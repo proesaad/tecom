1c9d64455cf58b073c16f4287311ddf2f281b585
Saad Elmilouay <79058058+proesaad@users.noreply.github.com>
2024-06-03 02:52:37 +0100

    Update Portfolio.js

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,30 +1,6 @@
 import React from "react";
 
 const Portfolio = ({ data }) => {
-  if (data) {
-    var projects = data.projects.map(function (projects) {
-      var projectImage = "images/portfolio/" + projects.image;
-      return (
-        <div key={projects.title} className="columns portfolio-item">
-          <div className="item-wrap">
-            <a href={projects.url} title={projects.title} target="_blank" rel="noreferrer" >
-              <img alt={projects.title} src={projectImage} />
-              <div className="overlay">
-                <div className="portfolio-item-meta">
-                  <h5>{projects.title}</h5>
-                  <p>{projects.category}</p>
-                </div>
-              </div>
-              <div className="link-icon">
-                <i className="fa fa-link"></i>
-              </div>
-            </a>
-          </div>
-        </div>
-      );
-    });
-  }
-
   return (
     <section id="portfolio">
       <div className="row">
